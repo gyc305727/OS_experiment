d25661a7bb2bd2abff7c342e8562c6b541432910
gyc305727-ship-it <gyc305727@gmail.com>
2026-05-18 14:05:36 +0800

    23251004 完成实验2裸机环境和最小化内核

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,68 +1,29 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
-use core::arch::asm;
-use core::fmt::{self, Write};
-use core::panic::PanicInfo;
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
 
-const SYSCALL_WRITE: usize = 64;
-const SYSCALL_EXIT: usize = 93;
+use core::arch::global_asm;
 
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let ret: isize;
-    unsafe {
-        asm!(
-            "ecall",
-            inlateout("a0") args[0] => ret,
-            in("a1") args[1],
-            in("a2") args[2],
-            in("a7") id,
-        );
+global_asm!(include_str!("entry.asm"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
     }
-    ret
-}
-
-fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
-}
-
-fn sys_exit(exit_code: i32) -> ! {
-    syscall(SYSCALL_EXIT, [exit_code as usize, 0, 0]);
-    loop {}
-}
-
-struct Stdout;
-
-impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        sys_write(1, s.as_bytes());
-        Ok(())
-    }
-}
-
-fn print(args: fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap();
-}
-
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        print(format_args!($fmt $(, $($arg)+)?));
-    }
-}
-
-macro_rules! println {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe {
+        (a as *mut u8).write_volatile(0);
+    });
 }
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub fn rust_main() -> ! {
+    clear_bss();
     println!("Hello, world!");
-    sys_exit(0);
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    panic!("Shutdown machine!");
 }
